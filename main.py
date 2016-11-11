2ac6a77362a2add146b5504ce12bd93ea2589b72
a-mile <und3fnd@outlook.com>
2016-11-11 15:51:28 +0100

    Hsv thresholding instead of gray thresholding

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import numpy as np
 import cv2
 import os
 
+def nothing(x):
+    pass
+
+cv2.namedWindow('image')
+cv2.createTrackbar('threshold1','image',0,255,nothing)
+cv2.createTrackbar('threshold2','image',0,255,nothing)
+
 dir = os.path.dirname(__file__)
 imageNumber = 1
 imageCount = 11
@@ -9,13 +16,13 @@ imageCount = 11
 while(1):
     filename = dir + '/samples/sample'+str(imageNumber)+'.jpg'
 
-    image = cv2.imread(filename,0)
+    image = cv2.imread(filename)
 
     blur = cv2.GaussianBlur(image, (15, 15), 0)
-    thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 5, 1)
-    kernel = np.ones((3, 3), np.uint8)
-    closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=4)
-    cont_img = closing.copy()
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    thresh = cv2.inRange(hsv,(cv2.getTrackbarPos('threshold1','image'),0,0),(cv2.getTrackbarPos('threshold2','image'),255,255))
+
+    cont_img = thresh.copy()
     _, contours, hierarchy = cv2.findContours(cont_img, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     for cnt in contours:
@@ -24,7 +31,7 @@ while(1):
             continue
         cv2.drawContours(image, [cnt], 0, (0, 255, 0), 3)
 
-    cv2.imshow('image',closing)
+    cv2.imshow('image',image)
 
     key = cv2.waitKey(1) & 0xFF
     if key == 27:
