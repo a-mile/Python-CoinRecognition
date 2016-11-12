51c46114608f2af8d1173832142d054c7ad43f37
a-mile <und3fnd@outlook.com>
2016-11-12 10:55:56 +0100

    Convex hull search approach

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ def nothing(x):
     pass
 
 cv2.namedWindow('image')
-cv2.createTrackbar('threshold1','image',0,255,nothing)
-cv2.createTrackbar('threshold2','image',0,255,nothing)
+cv2.createTrackbar('C','image',0,10,nothing)
+cv2.createTrackbar('Opening iterations','image',0,10,nothing)
 
 dir = os.path.dirname(__file__)
 imageNumber = 1
 imageCount = 11
+showImage = True
 
 while(1):
     filename = dir + '/samples/sample'+str(imageNumber)+'.jpg'
@@ -19,19 +20,38 @@ while(1):
     image = cv2.imread(filename)
 
     blur = cv2.GaussianBlur(image, (15, 15), 0)
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    thresh = cv2.inRange(hsv,(cv2.getTrackbarPos('threshold1','image'),0,0),(cv2.getTrackbarPos('threshold2','image'),255,255))
+
+    gray = hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+
+    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 15, (cv2.getTrackbarPos('C','image')))
+    kernel = np.ones((3, 3), np.uint8)
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=(cv2.getTrackbarPos('Opening iterations','image')))
 
     cont_img = thresh.copy()
-    _, contours, hierarchy = cv2.findContours(cont_img, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    _, contours, hierarchy = cv2.findContours(cont_img, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
     for cnt in contours:
         area = cv2.contourArea(cnt)
         if area < 2000:
             continue
-        cv2.drawContours(image, [cnt], 0, (0, 255, 0), 3)
+        hull = cv2.convexHull(cnt)
+        hullf = cv2.convexHull(cnt, returnPoints=False)
+        defects = cv2.convexityDefects(cnt, hullf)
 
-    cv2.imshow('image',image)
+        for i in range(defects.shape[0]):
+            s, e, f, d = defects[i, 0]
+            start = tuple(cnt[s][0])
+            end = tuple(cnt[e][0])
+            far = tuple(cnt[f][0])
+            cv2.circle(image, far, 5, [0, 0, 255], -1)
+
+        cv2.drawContours(image, [hull], -1, (0, 255, 0), 2)
+        #cv2.drawContours(image, [cnt], 0, (255, 255, 0), 3)
+
+    if showImage:
+        cv2.imshow('image',image)
+    else:
+        cv2.imshow('image', thresh)
 
     key = cv2.waitKey(1) & 0xFF
     if key == 27:
@@ -44,5 +64,7 @@ while(1):
         imageNumber = imageNumber - 1
         if imageNumber < 1:
             imageNumber = 1
+    if key == 1:
+        showImage = not showImage
 
 cv2.destroyAllWindows()
